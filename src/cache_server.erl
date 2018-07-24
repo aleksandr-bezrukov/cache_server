21557d30d691eb3c57e943ede77babb4cb89c01b
Aleksandr Bezrukov <bezrukov.aleksandr.victorovich@gmail.com>
2018-07-24 07:28:04 +0300

    2

--- src/cache_server.erl
+++ src/cache_server.erl
@@ -1,6 +1,6 @@
 -module(cache_server).
 
--export([start/1, stop/0, get_data/1, put_data/2, get_by_date/2, cleaning/0]).
+-export([start/1, stop/0, lookup/1, insert/3, lookup_by_date/2, cleaning/0]).
 
 -behaviour(gen_server).
 %% gen_server callbacks
@@ -12,16 +12,67 @@
         connections = orddict:new() :: orddict:orddict(file:name_all(), port())
          }).
 
+-define(CACHENAME, ?MODULE).
+
     start([{drop_interval, Seconds}]) ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, [{drop_interval, Seconds}], []).
 
-    stop() -> gen_server:call(?MODULE, stop).
+    stop() -> gen_server:call(?MODULE, {stop}).
 
     insert(Key, Value, Seconds) -> gen_server:call(?MODULE,{insert,Key,Value,Seconds}).
 
     lookup(Key) -> gen_server:call(?MODULE,{find,Key}).
 
     lookup_by_date(DateFrom, DateTo) -> gen_server:call(?MODULE,{find_period,DateFrom, DateTo}).
+
+    cleaning() ->
+	Now = os:system_time(second)
+	,delete_expired(ets:select(CACHENAME,[{{'$1','$2','$3'},[{'<','$3',Now}],[['$1']
+    delete_expired(ExpiredList) ->
+	case ExpiredList of
+    <------>[] -> ok;
+<---	--->[[H]|T] -> ets:delete(CACHENAME,H)
+<---	--->,delete_expired(T)
+	end.
+
+    init([{drop_interval, DrTime}]) ->
+        ok = timer:start()
+	,{ok, _TRef} = timer:apply_interval(DrTime*1000, cache_server, cleaning, []),
+	,{ok, ets:new(CACHENAME,[public,named_table])}.
+
+    handle_call({insert,Key,Value,Seconds},_From,State) ->
+	
+	,Reply = ets:insert(CACHENAME,{Key,Value,Seconds+Now)})
+	,{reply, Reply, State}.
+
+
+
+    handle_call({find,Key}, _From, State) ->
+	Now = calendar:datetime_to_gregorian_seconds({date(), time()})
+	,Reply = case ets:lookup(CACHENAME,Key) of
+		    [] -> []
+		    ;[{_,_,TimeOfRecord}] when Now > TimeOfRecord -> []
+		    ;[{_,Value,_}] -> Value
+		 end
+	,{reply, Reply, State}.
+
+    handle_call({find_period,DateFrom, DateTo}, _From, State) ->
+	FromSec = calendar:datetime_to_gregorian_seconds(DateFrom)
+	,ToSec = calendar:datetime_to_gregorian_seconds(DateTo)
+	,Reply = get_in_period(ets:first(CACHENAME), FromSec, ToSec, [])
+	,{reply, Reply, Tab}.
+
+
+
+	get_in_period(Next,From,To,Acc) ->
+	    case Next of
+    handle_call({insert,Key,Value,Seconds}, _From, State) ->
+	Reply = ets:insert(CACHENAME,{Key,Value,Seconds+os:system_time(second)})
+	,{reply, Reply, State}.
+
+    handle_call({stop}, _From, State) ->
+	{stop, normal, stopped, State}.
+
 
 get_emo(Who) -> gen_server:call(?MODULE, {get, Who}).
 put_emo({Who, Status}, Time) -> gen_server:call(?MODULE, {put, Who, Status, Time}).
